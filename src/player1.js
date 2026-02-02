e2e56099a33e4398be43f216b0ee6546fcd861d7
Mohammed Haji Hussain <mohammedhajihussain@Mohammeds-MacBook-Pro.local>
2026-02-02 10:21:05 +0530

    adds data transfer between server and player
    * gives turn to players one after another
    * needs refactoring

--- src/player1.js
+++ src/player1.js
@@ -1,40 +1,85 @@
 const boxen = (word) => {
-  const topLine = `┌${'-'.repeat(word.length)}┐`;
-  const line = `|${' '.repeat(word.length)}|`
-  const lastLine = `└${'-'.repeat(word.length)}┘`;
+  const topLine = `┌${"-".repeat(word.length)}┐`;
+  const line = `|${" ".repeat(word.length)}|`;
+  const lastLine = `└${"-".repeat(word.length)}┘`;
   const middleLine = `|${word}|`;
   return [topLine, line, middleLine, line, lastLine];
-}
+};
 
 const addLines = (line1, line2) => {
   return line1.map((line, i) => `${line} ${line2[i]}`);
-}
+};
 
-const connection = await Deno.connect({
-  hostname: "192.168.1.59",
-  port: 8000,
-  transport: "tcp",
-});
+const connect = async () => {
+  return await Deno.connect({
+    hostname: "10.132.125.26",
+    port: 8000,
+    transport: "tcp",
+  });
+};
 
 const displayWords = (data) => {
-  const cards = data.map(word => boxen(word));
+  const cards = data.map((word) => boxen(word));
   const board = cards.reduce((line1, line2) => addLines(line1, line2));
-  console.log(board.join('\n'));
-}
+  console.log(board.join("\n"));
+};
 
-const decoder = new TextDecoder();
-const buffer = new Uint8Array(1024);
-let bytesRead = await connection.read(buffer);
-let data = buffer.slice(0, bytesRead);
-console.log(decoder.decode(data));
-
-bytesRead = await connection.read(buffer);
-data = buffer.slice(0, bytesRead);  
-displayWords(JSON.parse(decoder.decode(data)));
-
-while (true) {
+const printWelcome = async (connection, buffer, decoder) => {
   const bytesRead = await connection.read(buffer);
-  const data = buffer.slice(0, bytesRead);
-  if (bytesRead !== null)
-  console.log(decoder.decode(data));
-}
+  const msg = decoder.decode(buffer.slice(0, bytesRead));
+  console.clear();
+  console.log(msg);
+};
+
+const readWords = async (connection, buffer, decoder) => {
+  const bytesRead = await connection.read(buffer);
+  const words = decoder.decode(buffer.slice(0, bytesRead));
+  return JSON.parse(words);
+};
+
+const read = async (connection, decoder, buffer) => {
+  const bytesRead = await connection.read(buffer);
+  const data = decoder.decode(buffer.slice(0, bytesRead));
+  return [bytesRead, data];
+};
+
+const readHints = () => {
+  const hint1 = prompt(`hint 1: `);
+  const hint2 = prompt(`hint 2: `);
+  const hint3 = prompt(`hint 3: `);
+  return [hint1, hint2, hint3];
+};
+
+const main = async () => {
+  const connection = await connect();
+  const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
+  const buffer = new Uint8Array(1024);
+  await printWelcome(connection, buffer, decoder);
+  const words = await readWords(connection, buffer, decoder);
+  displayWords(words);
+  while (true) {
+    const [bytesRead, data] = await read(connection, decoder, buffer);
+    if (bytesRead !== null) {
+      if (data.startsWith("your Turn")) {
+        console.log(data.slice(9));
+        const hints = readHints();
+        await connection.write(encoder.encode(JSON.stringify(hints)));
+        console.clear();
+        displayWords(words);
+        hints.forEach((hint) => console.log(hint));
+        const code = prompt(`enter the code`);
+        console.log(`sending data....`);
+        await connection.write(encoder.encode(code));
+      } else {
+        const hints = JSON.parse(data);
+        hints.forEach((element) => console.log(element));
+        const code = prompt(`enter the code`);
+        console.log(`sending data....`);
+        await connection.write(encoder.encode(code));
+      }
+    }
+  }
+};
+
+await main();
--- src/server.js
+++ src/server.js
@@ -23,18 +23,43 @@ const getPlayers = async () => {
   }
 };
 
+const generateCode = () => {
+  return `3 1 2`;
+};
+
+const handle = async (player1, player2, encoder, decoder, buffer) => {
+  const code = generateCode();
+  await player1.write(encoder.encode(`your Turn`));
+  await player1.write(encoder.encode(`===== CODE =====\n      ${code}     `));
+  const bytes = await player1.read(buffer);
+  if (bytes !== null) {
+    console.log(decoder.decode(buffer.slice(0, bytes)));
+    await player2.write(buffer.slice(0, bytes));
+    let byteCount = await player1.read(buffer);
+    const player1Guess = decoder.decode(buffer.slice(0, byteCount));
+    byteCount = await player2.read(buffer);
+    const player2Guess = decoder.decode(buffer.slice(0, byteCount));
+    if (player1Guess.length && player2Guess.length);
+    console.log(player1Guess, player2Guess);
+  }
+};
+
 const play = async (player1, player2) => {
   const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+  const buffer = new Uint8Array(1024);
   const player1Words = getWords();
   const player2Words = getWords();
   await player1.write(encoder.encode(JSON.stringify(player1Words)));
   await player2.write(encoder.encode(JSON.stringify(player2Words)));
-  // let player1Points = 0;
-  // let player2Points = 0;
-  // let Rounds = 0;
-  // while (player1Points < 2 && player2Points < 2 && rounds < 8) {
-
-  // }
+  let player1Points = 0;
+  let player2Points = 0;
+  let rounds = 0;
+  while (rounds<8) {
+    await handle(player1, player2, encoder, decoder, buffer);
+    await handle(player2, player1, encoder, decoder, buffer);
+    rounds++;
+  }
 };
 
 const main = async () => {
